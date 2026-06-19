c1b98b81490b965922a4cb325224130a50a5afd0
hori <horihayato1128@gmail.com>
2026-06-19 22:27:57 +0900

    '到着機能'

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { getApps, initializeApp } from 'firebase/app';
+import type { Auth } from 'firebase/auth';
 import { getAuth, initializeAuth } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -12,7 +13,7 @@ const firebaseConfig = {
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 
-let auth;
+let auth: Auth;
 try {
   // Use dynamic require for `firebase/auth/react-native` so builds without its typings still work.
   // eslint-disable-next-line @typescript-eslint/no-var-requires
